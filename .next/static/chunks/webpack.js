2b6f6706b8c45b56cbd91f40c35d45cc33e0d501
AfrozonAuto <ola@panthergon.com>
2026-02-11 18:16:14 -0500

    Updated build-manifest.json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7cb0b5eb8d0abd08"; }
+/******/ 		__webpack_require__.h = function() { return "fd05a8f138a86962"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
